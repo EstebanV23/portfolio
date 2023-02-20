ace520755429e73b6cfc35ac668ee2a1d01f0323
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-02-19 23:39:21 -0500

    Fix responsive and add contact section

--- main.js
+++ main.js
@@ -7,32 +7,43 @@ const background = $('#parallaxBackground')
 const left = $('#parallaxLeft')
 const right = $('#mineRight')
 const gold = $('#mineGold')
-const texts = $('.presentation-texts')
 const textSection = $('#textSection')
+const nameSection = $('.name-section')
 
 modifyNav()
 
 let exeTyping = false
+let typingName = false
 let i = 0
+let j = 0
+
+const text = 'I\'m web developer, I can develop any aplication, with dedication and time, I sure that I will achieve it, I can control programming languages like, Node js, Java, Javascript, React and really any language that I propose me, I\'m able to learn quickly'
+const nameText = 'Brayan Esteban Villamizar Fernandez'
 
 window.addEventListener('scroll', () => {
   const value = window.scrollY * 0.3
   background.style.top = `${value}px`
   left.style.top = `${value}px`
   right.style.top = `${value}px`
-  const valueTexts = 90
-  value > valueTexts
-    ? texts.style.transform = `translate(${value}%, ${value * 1.3}px)`
-    : texts.style.transform = `translate(0%, ${value}px)`
 
   if (value >= textSection.offsetTop - 70 && !exeTyping) {
     exeTyping = true
-    typing()
+    typing(textSection, text, exeTyping, i)
   }
   if (value < textSection.offsetTop - 70) {
     exeTyping = false
     i = 0
     textSection.innerHTML = ''
+  }
+
+  if (value >= nameSection.offsetTop + 370 && !typingName) {
+    typingName = true
+    typing(nameSection, nameText, typingName, j)
+  }
+  if (value < nameSection.offsetTop + 370) {
+    typingName = false
+    j = 0
+    nameSection.innerHTML = ''
   }
 })
 
@@ -42,12 +53,11 @@ document.addEventListener('mousemove', (e) => {
   gold.style.transform = `translate(${x}px, ${y}px)`
 })
 
-const text = 'I\'m web developer, I can develop any aplication, with dedication and time, I sure that I will achieve it, I can control programming languages like, Node js, Java, Javascript, React and really any language that I propose me, I\'m able to learn quickly'
-
-function typing () {
-  if (i < text.length) {
-    textSection.innerHTML += text[i]
-    i++
-    setTimeout(typing, 20)
+function typing (textSelect, myText, executeTyping, iterator) {
+  if (!exeTyping && !typingName) return
+  if (iterator < myText.length) {
+    textSelect.innerHTML += myText[iterator]
+    iterator++
+    setTimeout(() => typing(textSelect, myText, executeTyping, iterator), 25)
   }
 }
